8821786206cacaa9303f99a25f242bdd469834c9
kenj-1 <dirkkenjibalili27@gmail.com>
2026-03-07 15:50:22 +0800

    init commit

--- src/encantadia/ui/ux/MainMenuFrame.java
+++ src/encantadia/ui/ux/MainMenuFrame.java
@@ -6,8 +6,7 @@ import java.awt.event.ActionListener;
 
 public class MainMenuFrame extends JFrame {
     private JPanel MainMenuFramePanel;
-    private JTextField textField1;
-    private JButton niggaButton;
+    private JButton arcadeButton;
     private JButton PVPButton;
     private JButton PVEButton;
 
@@ -16,10 +15,10 @@ public class MainMenuFrame extends JFrame {
         setContentPane(MainMenuFramePanel);
         setTitle("Encantadia: Echoes of the Gem - Main Menu");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(300, 300);
+        setSize(1024, 768);
         setLocationRelativeTo(null);
         setVisible(true);
-        niggaButton.addActionListener(new ActionListener() {
+        arcadeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JOptionPane.showMessageDialog(MainMenuFrame.this, "Hello all my niggas say what");
